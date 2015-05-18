d5ee6f0772cd60fc3c34b8e21c29fdf83b5e88f4
Jared Burrows <jaredsburrows@gmail.com>
2015-05-17 22:01:24 -0400

    Added new Gradle wrapper Added new .gitignore

---
+++ examples-old/ImageManipulation/obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o.d
@@ -0,0 +1,151 @@
+obj/local/armeabi-v7a/objs/mixed_sample/jni_part.o: jni/jni_part.cpp \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/assert.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/string.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/memory.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/math.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/limits.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ /Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/config.h
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/assert.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/math.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/noname/android-ndk/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/Users/noname/android-ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/features2d/features2d.hpp:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+/Users/noname/Downloads/OpenCV-2.4.8-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
